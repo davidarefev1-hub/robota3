3429456eb39328bdd6379e56293a3280553a3279
davidarefev1-hub <davidarefev1@gmail.com>
2026-09-24 11:50:17 +0300

    111

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,28 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.text.MessageFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("byte: " + Byte.SIZE + " біт [" + Byte.MIN_VALUE + " ... " + Byte.MAX_VALUE + "]");
-        System.out.println("short: " + Short.SIZE + " біт [" + Short.MIN_VALUE + " ... " + Short.MAX_VALUE + "]");
-        System.out.println("int: " + Integer.SIZE + " біт [" + Integer.MIN_VALUE + " ... " + Integer.MAX_VALUE + "]");
-        System.out.println("long: " + Long.SIZE + " біт [" + Long.MIN_VALUE + " ... " + Long.MAX_VALUE + "]");
-        System.out.println("float: " + Float.SIZE + " біт [" + Float.MIN_VALUE + " ... " + Float.MAX_VALUE + "]");
-        System.out.println("double: " + Double.SIZE + " біт [" + Double.MIN_VALUE + " ... " + Double.MAX_VALUE + "]");
-        System.out.println("char: " + Character.SIZE + " біт [" + (int)Character.MIN_VALUE + " ... " + (int)Character.MAX_VALUE + "]");
-        System.out.println("boolean: 1 біт [false / true]\n");
-
         Scanner sc = new Scanner(System.in);
+        System.out.print("Введите int, double, String, boolean через пробел: ");
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        String s = sc.next();
+        boolean b = sc.nextBoolean();
 
-        System.out.print("byte: ");    byte b = Byte.parseByte(sc.nextLine());
-        System.out.print("short: ");   short s = Short.parseShort(sc.nextLine());
-        System.out.print("int: ");     int i = Integer.parseInt(sc.nextLine());
-        System.out.print("long: ");    long l = Long.parseLong(sc.nextLine());
-        System.out.print("float: ");   float f = Float.parseFloat(sc.nextLine());
-        System.out.print("double: ");  double d = Double.parseDouble(sc.nextLine());
-        System.out.print("boolean: "); boolean bool = Boolean.parseBoolean(sc.nextLine());
-        System.out.print("char: ");    char c = sc.nextLine().charAt(0);
+        System.out.printf("1: %d | %.2f | %s | %b%n", i, d, s, b);
+        System.out.printf("2: Dec=%d | Hex=0x%X | Oct=0%o%n", i, i, i);
+        System.out.printf("3: [%10.4f]%n", d);
+        System.out.printf("4: [%-15.5s]%n", s);
 
-        System.out.println("Отримано: " + b + ", " + s + ", " + i + ", " + l + ", " + f + ", " + d + ", " + bool + ", " + c);
+        System.out.println(MessageFormat.format("5: String=\"{0}\", Bool={1}, Int={2}", s, b, i));
+        System.out.println(MessageFormat.format("6: Int={0,number,integer}, Dbl={1,number,#,##0.000}", i, d));
+        System.out.println(MessageFormat.format("7: Percent={0,number,percent}", d));
+
+        System.out.println("8: %08d | %.1f".formatted(i, d));
+        System.out.println("9: [%20S]".formatted(s));
+        System.out.println("10: %+d | %B".formatted(i, b));
     }
 }
